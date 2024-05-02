4cca2a89f77540078d4c0ff537f06155b1162876
Course Files <137632706+course-files@users.noreply.github.com>
2024-05-02 12:53:14 -0400

    Update

--- Join-Order-Benchmark-queries--/2c.sql
+++ Join-Order-Benchmark-queries--/2c.sql
@@ -1,10 +1,11 @@
-SELECT MIN(t.title) AS movie_title
+-- JOB Query 2c
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE/*, FORMAT YAML*/) SELECT MIN(t.title) AS movie_title
 FROM company_name AS cn,
      keyword AS k,
      movie_companies AS mc,
      movie_keyword AS mk,
      title AS t
-WHERE cn.country_code ='[us]'
+WHERE cn.country_code ='[sm]'
   AND k.keyword ='character-name-in-title'
   AND cn.id = mc.company_id
   AND mc.movie_id = t.id
--- Join-Order-Benchmark-queries--/2d.sql
+++ Join-Order-Benchmark-queries--/2d.sql
@@ -1,10 +1,11 @@
-SELECT MIN(t.title) AS movie_title
+-- JOB Query 2d
+EXPLAIN (ANALYZE, BUFFERS, VERBOSE/*, FORMAT YAML*/) SELECT MIN(t.title) AS movie_title
 FROM company_name AS cn,
      keyword AS k,
      movie_companies AS mc,
      movie_keyword AS mk,
      title AS t
-WHERE cn.country_code ='[sm]'
+WHERE cn.country_code ='[us]'
   AND k.keyword ='character-name-in-title'
   AND cn.id = mc.company_id
   AND mc.movie_id = t.id
