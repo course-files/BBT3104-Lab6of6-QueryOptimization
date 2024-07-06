accf1a0fa706ac49176b386aca4cb40bde357e4f
Course Files <137632706+course-files@users.noreply.github.com>
2024-07-06 06:00:29 +0300

    Update schema.sql

--- Internet-Movie-Database--IMBDb/schema.sql
+++ Internet-Movie-Database--IMBDb/schema.sql
@@ -4,8 +4,6 @@ CREATE DATABASE imdb WITH
 OWNER = postgres
 TEMPLATE = template0
 ENCODING = 'UTF8'
-LC_COLLATE = 'en_GB.utf8'
-LC_CTYPE = 'en_GB.utf8'
 CONNECTION LIMIT = 10;
 
 -- SCHEMA: imdb_schema
