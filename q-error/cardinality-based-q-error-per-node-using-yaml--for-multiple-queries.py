71d03c23c131537f15ff77b3c551aa2c7e73e29a
Course Files <137632706+course-files@users.noreply.github.com>
2024-08-13 16:30:37 +0300

    Update cardinality-based-q-error-per-node-using-yaml--for-multiple-queries.py

--- q-error/cardinality-based-q-error-per-node-using-yaml--for-multiple-queries.py
+++ q-error/cardinality-based-q-error-per-node-using-yaml--for-multiple-queries.py
@@ -77,7 +77,8 @@ def main():
     # if you are running it from the 'q-error' directory.
 
     # file_path = 'Join-Order-Benchmark-queries/1a.sql'
-    file_path = 'Join-Order-Benchmark-queries/JOB-light-70.sql'
+    # file_path = 'Join-Order-Benchmark-queries/JOB-light-70.sql'
+    file_path = 'Join-Order-Benchmark-queries/JOB-light-70-delete.sql'
     # file_path = 'Join-Order-Benchmark-queries/JOB-light-70.sql'
     
     queries = read_queries_from_file(file_path)
