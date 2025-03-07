b37f5aa544b478f0dec2b215e92b9aac2e1c04ba
Allan Omondi <aomondi@strathmore.edu>
2025-03-07 10:15:23 +0300

    Update JOB-scale-500-pending-for-mysql.sql

--- Join-Order-Benchmark-queries/JOB-scale-500-pending-for-mysql.sql
+++ Join-Order-Benchmark-queries/JOB-scale-500-pending-for-mysql.sql
@@ -1,3 +1,4 @@
+/* JOB-scale-500-pending-for-mysql.sql */
 SELECT * FROM title t,cast_info ci,movie_info mi,movie_info_idx mi_idx,movie_keyword mk WHERE t.id=ci.movie_id AND t.id=mi.movie_id AND t.id=mi_idx.movie_id AND t.id=mk.movie_id AND mi.info_type_id>15;
 SELECT * FROM title t,cast_info ci,movie_info mi,movie_info_idx mi_idx,movie_keyword mk WHERE t.id=ci.movie_id AND t.id=mi.movie_id AND t.id=mi_idx.movie_id AND t.id=mk.movie_id AND mi.info_type_id>4;
 SELECT * FROM title t,movie_info mi,movie_info_idx mi_idx WHERE t.id=mi.movie_id AND t.id=mi_idx.movie_id AND mi_idx.info_type_id<101;
